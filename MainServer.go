a128e1ebb28d1ec76d4796917880f1a61a290414
unknown <kevin.fu.95@gmail.com>
2020-06-17 19:28:26 -0400

    renaming

--- MainServer.go
+++ MainServer.go
@@ -1,42 +1,37 @@
 package main
 
 import (
-  
-  //"os"
-  "net/http"
-  "github.com/gorilla/mux"
-  common "./common"
-  "log"
-  "github.com/jinzhu/gorm"
-  _ "github.com/jinzhu/gorm/dialects/postgres"
-  singleton "./singleton"
+
+	//"os"
+	"log"
+	"net/http"
+
+	controller "./controller"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 var router = mux.NewRouter()
 
-func main(){
+func main() {
 
+	db, err := gorm.Open("postgres", "host=localhost port=5432 user=kevinfu dbname=mydb password=12345 sslmode=disable")
+	if err != nil {
+		panic("failed to connect database")
 
-  db, err := gorm.Open("postgres", "host=localhost port=5432 user=kevinfu dbname=mydb password=12345 sslmode=disable")
-  if err != nil {
-    panic("failed to connect database")
+	}
+	singleton.setDB(db)
 
-  }
-  singleton.setDB(db)
+	router.HandleFunc("/", controller.MainPageHandler)
+	router.HandleFunc("/login", controller.LoginPageHandler).Methods("GET")
+	router.HandleFunc("/login", controller.LoginHandler).Methods("POST")
+	router.HandleFunc("/register", controller.RegisterPageHandler).Methods("GET")
+	router.HandleFunc("/register", controller.RegisterHandler).Methods("POST")
+	//router.HandleFunc("/logout",controller.LogoutHandler).Methods("POST")
 
-  
+	http.Handle("/", router)
 
-  router.HandleFunc("/",common.MainPageHandler)
-  router.HandleFunc("/login", common.LoginPageHandler).Methods("GET")
-  router.HandleFunc("/login", common.LoginHandler).Methods("POST")
-  router.HandleFunc("/register",common.RegisterPageHandler).Methods("GET")
-  router.HandleFunc("/register",common.RegisterHandler).Methods("POST")
-  //router.HandleFunc("/logout",common.LogoutHandler).Methods("POST")
-
-
-  http.Handle("/", router)
-
-  log.Fatal(http.ListenAndServe(":8082",nil))
-
+	log.Fatal(http.ListenAndServe(":8082", nil))
 
 }
--- controller/Handlers.go
+++ controller/Handlers.go
@@ -1,49 +1,50 @@
 package common
 
-import(
+import (
 	"fmt"
 	"net/http"
-	helpers "../helpers"
+
 	datamodel "../datamodel"
+	helpers "../helpers"
 )
-
-
 
 // For initial loading of page
 
-// load of the log in page
+//handles loading main page
 func MainPageHandler(response http.ResponseWriter, request *http.Request) {
-    var body, _ = helpers.LoadFile("templates/index.html")
-    fmt.Fprintf(response, body)
+	var body, _ = helpers.LoadFile("templates/index.html")
+	fmt.Fprintf(response, body)
 }
 
 func LoginPageHandler(response http.ResponseWriter, request *http.Request) {
-    var body, _ = helpers.LoadFile("templates/login.html")
-    fmt.Fprintf(response, body)
+	var body, _ = helpers.LoadFile("templates/login.html")
+	fmt.Fprintf(response, body)
 }
-// handle the post request 
-func LoginHandler(response http.ResponseWriter, request *http.Request){
+
+// handle the post request
+
+func LoginHandler(response http.ResponseWriter, request *http.Request) {
 	name := request.FormValue("name")
 	pass := request.FormValue("password")
-	
+
 	fmt.Printf("username is:  %s \n password is:  %s  \n", name, pass)
-	http.Redirect(response, request ,"/", 302)
+	http.Redirect(response, request, "/", 302)
 }
 
 func RegisterPageHandler(response http.ResponseWriter, request *http.Request) {
-    var body, _ = helpers.LoadFile("templates/register.html")
-    fmt.Fprintf(response, body)
+	var body, _ = helpers.LoadFile("templates/register.html")
+	fmt.Fprintf(response, body)
 }
 
-func RegisterHandler(response http.ResponseWriter, request *http.Request){
+func RegisterHandler(response http.ResponseWriter, request *http.Request) {
 	name := request.FormValue("name")
 	email := request.FormValue("email")
 	pass := request.FormValue("password")
 	passCon := request.FormValue("confirmPassword")
 
 	fmt.Printf("username is: %s \n password is:  %s \n passwordConfirmed is: %s \n email is: %s \n ", name, pass, passCon, email)
-	aUser := datamodel.User{name,email,pass}
+	aUser := datamodel.User{name, email, pass}
 	datamodel.DBCheckWrite(&aUser)
 
-	http.Redirect(response, request, "/",302)
+	http.Redirect(response, request, "/", 302)
 }
